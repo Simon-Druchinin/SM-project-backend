b3cef91ba6ea894c831cfed85134f001755475ab
Simon <semastar00@mail.ru>
2023-08-21 07:52:38 +0700

    develop | Added Dockerfile & docker-compose

--- src/main.py
+++ src/main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
 
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from fastapi_cache.decorator import cache
+
+from redis import asyncio as aioredis
+
 from src.auth.config import auth_backend, fastapi_users
 from src.auth.schemas import UserRead, UserCreate
 
@@ -22,3 +28,8 @@ app.include_router(
 )
 
 app.include_router(menu_router)
+
+@app.on_event("startup")
+async def startup_event():
+    redis = aioredis.from_url("redis://localhost", encoding="utf8", decode_responses=True)
+    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
